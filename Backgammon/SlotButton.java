77a6357ca4840f6e347eccd0c723a5726c166d24
jcwasher <62572358+jcwasher@users.noreply.github.com>
2020-04-09 03:29:52 -0400

    Added two color set functions
    -- can now set background color for all SlotButtons and triangle color when applicable

--- Backgammon/SlotButton.java
+++ Backgammon/SlotButton.java
@@ -1,8 +1,12 @@
 //SlotButton Panel Constructor
 import javax.swing.JButton;
-
+import java.awt.Color;
 //Abstract class so we can have many buttons
 //in an array but with many different appearances
 public abstract class SlotButton extends JButton
 {
+	//Sets background color
+	public abstract void setBkgdColor(Color b);
+  //Sets triangle color (if applicable)
+	public abstract void setTriColor(Color t);
 }
