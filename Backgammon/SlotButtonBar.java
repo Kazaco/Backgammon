fa98bd231bf57f8bea25ea80a535cc1fe5f92e06
jcwasher <62572358+jcwasher@users.noreply.github.com>
2020-04-09 03:37:05 -0400

    Defined new color setters and played with colors

--- Backgammon/SlotButtonBar.java
+++ Backgammon/SlotButtonBar.java
@@ -4,13 +4,28 @@ import javax.swing.JButton;
 import java.awt.Graphics;
 import java.awt.Color;
 
-public class SlotButtonBar extends SlotButton{
-
-    public SlotButtonBar(String text)
+public class SlotButtonBar extends SlotButton
+{
+	private Color background;
+    
+	public SlotButtonBar(String text)
     {
         //Call Parent Constructor
         super();
+		background = new Color(85,60,42);
     }
+	
+	public void setBkgdColor(Color b)
+	{
+		background = b;
+		repaint();
+	}
+	
+	//No definition needed
+	public void setTriColor(Color t)
+	{
+		
+	}
 
     public void paintComponent(Graphics g)
     {
@@ -18,6 +33,6 @@ public class SlotButtonBar extends SlotButton{
         super.paintComponent(g);
 
         //Make bar the same color as the border
-        setBackground(Color.ORANGE);
+        setBackground(background);
     }
 }
