94594537203fa95f230aeee568a267bf022791c7
jcwasher <62572358+jcwasher@users.noreply.github.com>
2020-04-09 03:45:52 -0400

    Defined new color setters and played with colors

--- Backgammon/SlotButtonPlayerTop.java
+++ Backgammon/SlotButtonPlayerTop.java
@@ -4,13 +4,31 @@ import javax.swing.JButton;
 import java.awt.Graphics;
 import java.awt.Color;
 
-public class SlotButtonPlayerTop extends SlotButton{
-
+public class SlotButtonPlayerTop extends SlotButton
+{
+	private Color background;
+	private Color triangle;
+	
     public SlotButtonPlayerTop(String text)
     {
         //Call Parent Constructor
         super();
+		//Default colors
+		background = new Color(32,32,32);
+		triangle = new Color(206,184,136);
     }
+
+	public void setBkgdColor(Color b)
+	{
+		background = b;
+		repaint();
+	}
+	
+	public void setTriColor(Color t)
+	{
+		triangle = t;
+		repaint();
+	}
 
     public void paintComponent(Graphics g)
     {
@@ -18,11 +36,12 @@ public class SlotButtonPlayerTop extends SlotButton{
         super.paintComponent(g);
 
         //Background for board on player slot on top of board
-        setBackground(Color.BLACK);
+        setBackground(background);
 
         //Points to draw triangle for each panel closest to player
         int xPoints[] = {this.getWidth()/16, this.getWidth()/2, this.getWidth() - this.getWidth()/16};
         int yPoints[] = {0, this.getHeight() - this.getHeight()/4, 0};
+		g.setColor(triangle);
         g.fillPolygon(xPoints, yPoints, 3);
     }
 }
